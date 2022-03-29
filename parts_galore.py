f2e84b095a83d0b13decda0725a5a2b41a481715
Alison Chan <alisonc@alisonc.net>
2022-03-28 20:23:17 -0400

    remove unused imports

--- parts_galore.py
+++ parts_galore.py
@@ -1,12 +1,6 @@
 #!/usr/bin/env python3
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.select import Select
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.common.exceptions import NoSuchElementException
-from datetime import datetime
-import time
-import re
 
 driver = webdriver.Chrome()
 driver.maximize_window()
